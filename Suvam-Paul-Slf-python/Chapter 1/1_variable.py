8da58356158f89d66b770c12bea34d555b55c7d1
Suvam Paul <suvampersonal555@gmail.com>
2025-01-22 10:42:43 +0530

    Add files via upload
    Git initialized

---
+++ Suvam-Paul-Slf-python/Chapter 1/1_variable.py
@@ -0,0 +1,5 @@
+print("hello world")
+suvam67 = 10.99
+print(29 * suvam67)
+
+_hello = "Suvam"
---
+++ Suvam-Paul-Slf-python/Chapter 1/2.variables & datatype.py
@@ -0,0 +1,19 @@
+a = 5
+b = "Suvam"
+
+p = type(b)
+print(p)
+
+# type conversion possible if the the conversion is valid
+
+a = "100"
+print(type(a))
+print(a)
+
+
+c = int(a)
+print(type(c))
+print(c)
+
+a = 31
+print(float(a))
